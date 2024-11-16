eaf123d04f33b9f7e852334e2d84f052d77e80a0
Rubab Mahmood <rubab.mehmod@gmail.com>
2024-11-16 21:48:02 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter , Routes,Route } from 'react-router-dom'
+import Profile from './Pages/Profile';
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp';
+import About from './Pages/About'
 function App() {
-  return (
-    <div className="App">
-      <p className=''>hi there</p>
-    </div>
+  return ( 
+  <BrowserRouter> 
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+  </Routes>
+</BrowserRouter>
   );
+  
 }
+
 
 export default App;
